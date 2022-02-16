cd863f1fb388aead4adefdba6a6e4f6e85df33f2
mathis patenaude <mathis9039@gmail.com>
2022-02-16 13:06:20 -0500

    commencenement de QuizActivity

--- app/src/main/java/com/example/quiz/QuizActivity.java
+++ app/src/main/java/com/example/quiz/QuizActivity.java
@@ -3,6 +3,8 @@ package com.example.quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        final ImageView retourBtn = findViewById(R.id.retourBtn);
+        final TextView timer = findViewById(R.id.timer);
+
+        final TextView nomSujet = findViewById(R.id.nomSujet);
+        final String getSujetChoisi = getIntent().getStringExtra("SujetChoisi");
+
+        nomSujet.setText(getSujetChoisi);
     }
 }
