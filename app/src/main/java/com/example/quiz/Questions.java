c39b7ffa4553713715fbad833c9fef427f3dbe33
mathis patenaude <mathis9039@gmail.com>
2022-02-18 15:08:30 -0500

    fix and push après que Android studio a freeze et perdu mes dernière modif

--- app/src/main/java/com/example/quiz/Questions.java
+++ app/src/main/java/com/example/quiz/Questions.java
@@ -9,7 +9,7 @@ public class Questions {
 
         final List<QuestionList> questionsList = new ArrayList<>();
 
-        // Create object of QuestionsList class and pass a questions along with options and answer
+        // Ordre des question: question-options-réponse
         final QuestionList question1 = new QuestionList("Quelle est la taille de la varaiable int?", "16 bit", "8 bit", "32 bit", "64 bit", "32 bit");
         final QuestionList question2 = new QuestionList("Quelle est la valeur par défault de la variable booléenne?", "true", "false", "null", "not defined", "false");
         final QuestionList question3 = new QuestionList("Quelle est la valeur par défaut d'une variable d'instance?", "Dépend du type de variable", "null", "0", "non attribué", "Dépend du type de variable");
@@ -17,7 +17,6 @@ public class Questions {
         final QuestionList question5 = new QuestionList("Lequel des éléments suivants n'est PAS un mot-clé ou un mot réservé en Java?", "if", "then", "goto", "while", "then");
         final QuestionList question6 = new QuestionList("Quel concept de Java est un moyen de convertir des objets du monde réel en termes de classe?", "Polymorphisme", "Encapsulation", "Abstraction", "Héritage", "Abstraction");
 
-        // add all questions to List<QuestionsList>
         questionsList.add(question1);
         questionsList.add(question2);
         questionsList.add(question3);
@@ -28,20 +27,17 @@ public class Questions {
         return questionsList;
     }
 
-    private static List<QuestionList> reactQuestions() {
+    private static List<QuestionList> htmlQuestions() {
 
         final List<QuestionList> questionsLists = new ArrayList<>();
 
-        // Create object of QuestionsList class and pass a questions along with options and answer
-        final QuestionList question1 = new QuestionList("?", "", "", "", "", "");
-        final QuestionList question2 = new QuestionList("?", "", "", "", "", "");
-        final QuestionList question3 = new QuestionList("?", "", "", "", "", "");
-        final QuestionList question4 = new QuestionList("?", "", "", "", "", "");
-        final QuestionList question5 = new QuestionList("?", "", "", "", "", "");
-        final QuestionList question6 = new QuestionList("?", "", "", "", "", "");
+        final QuestionList question1 = new QuestionList("Que veut dire HTML?", "Hyper Text Markup Language", "High Text Markup Language", "Hyper Tabular Markup Language", "Aucune de ces réponses", "Hyper Text Markup Language");
+        final QuestionList question2 = new QuestionList("Quelle balise utilise-t'on pour déclarer un paragraphe?", "<TD>", "<br>", "<P>", "<TR>", "<P>");
+        final QuestionList question3 = new QuestionList("Quelle balise utilise-t'on pour déclarer une liste descriptive?", "<LL>", "<DD>", "<DL>", "<DS>", "<DL>");
+        final QuestionList question4 = new QuestionList("Quelle balise utilise-t'on pour déclarer le plus gros titre", "<head>", "<large>", "<h6>", "<heading>", "<h1>");
+        final QuestionList question5 = new QuestionList("La balise HTML qui spécifie un style CSS intégré dans un élément est appelée", "Design", "Style", "src", "Define", "Style");
+        final QuestionList question6 = new QuestionList("HTML est considéré comme", "Langage de programmation", "Langage POO", "Le language le plus puissant sur Terre", "Langage de balisage", "Langage de balisage");
 
-
-        // add all questions to List<QuestionsList>
         questionsLists.add(question1);
         questionsLists.add(question2);
         questionsLists.add(question3);
@@ -52,19 +48,18 @@ public class Questions {
         return questionsLists;
     }
 
-    private static List<QuestionList> htmlQuestions() {
+    private static List<QuestionList> reactQuestions() {
 
         final List<QuestionList> questionsLists = new ArrayList<>();
 
-        // Create object of QuestionsList class and pass a questions along with options and answer
-        final QuestionList question1 = new QuestionList("Que veut dire HTML?", "Hyper Text Markup Language", "High Text Markup Language", "Hyper Tabular Markup Language", "Aucune de ces réponses", "Hyper Text Markup Language");
-        final QuestionList question2 = new QuestionList("Quelle balise utilise-t'on pour déclarer un paragraphe?", "<TD>", "<br>", "<P>", "<TR>", "<P>");
-        final QuestionList question3 = new QuestionList("Quelle balise utilise-t'on pour déclarer une liste descriptive?", "<LL>", "<DD>", "<DL>", "<DS>", "<DL>");
-        final QuestionList question4 = new QuestionList("Quelle balise utilise-t'on pour déclarer le plus gros titre", "<head>", "<large>", "<h6>", "<heading>", "<h1>");
-        final QuestionList question5 = new QuestionList("La balise HTML qui spécifie un style CSS intégré dans un élément est appelée", "Design", "Style", "src", "Define", "Style");
-        final QuestionList question6 = new QuestionList("HTML est considéré comme", "Langage de programmation", "Langage POO", "Le language le plus puissant sur Terre", "Langage de balisage", "Langage de balisage");
+        final QuestionList question1 = new QuestionList("?", "", "", "", "", "");
+        final QuestionList question2 = new QuestionList("?", "", "", "", "", "");
+        final QuestionList question3 = new QuestionList("?", "", "", "", "", "");
+        final QuestionList question4 = new QuestionList("?", "", "", "", "", "");
+        final QuestionList question5 = new QuestionList("?", "", "", "", "", "");
+        final QuestionList question6 = new QuestionList("?", "", "", "", "", "");
 
-        // add all questions to List<QuestionsList>
+
         questionsLists.add(question1);
         questionsLists.add(question2);
         questionsLists.add(question3);
@@ -79,7 +74,6 @@ public class Questions {
 
         final List<QuestionList> questionsLists = new ArrayList<>();
 
-        // Create object of QuestionsList class and pass a questions along with options and answer
         final QuestionList question1 = new QuestionList("?", "", "", "", "", "");
         final QuestionList question2 = new QuestionList("?", "", "", "", "", "");
         final QuestionList question3 = new QuestionList("?", "", "", "", "", "");
@@ -87,7 +81,6 @@ public class Questions {
         final QuestionList question5 = new QuestionList("?", "", "", "", "", "");
         final QuestionList question6 = new QuestionList("?", "", "", "", "", "");
 
-        // add all questions to List<QuestionsList>
         questionsLists.add(question1);
         questionsLists.add(question2);
         questionsLists.add(question3);
